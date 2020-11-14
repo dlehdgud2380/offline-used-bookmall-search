fe590ac0404f7ac42db72220fe9f4894131aa5af
dlehdgud2380 <dlehdgud2380@gmail.com>
2020-11-14 17:32:59 +0900

    20201114 - module from branch/wip

--- module/aladin.py
+++ module/aladin.py
@@ -4,9 +4,12 @@ Aladin OfflineShop Parse Module by Sc0_Nep
 
 from bs4 import BeautifulSoup
 import requests
+import json
 
+#알라딘 고정 URL
 URL = "https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=UsedStore&KeyTag=&SearchWord="
 
+#알라딘 검색 페이지 크롤링 클래스
 class Searchpage:
     def __init__(self, keyword):
         html = requests.get(URL + keyword).text
@@ -14,81 +17,99 @@ class Searchpage:
         self.get_items = self.soup.find_all("div", class_ = "ss_book_box")
         self.item_quantity = len(self.get_items)
 
-        #Essential lists
+        #책제목, 책 설명, 재고가 있는 매장
         self.list_title = []
         self.list_description = []
         self.list_shop = []
 
-        #Parse & arrange data
-        self.__parse_data()
+        #검색페이지 크롤링 실행
+        self.__parse_searchdata()
 
-    def __parse_data(self):
+    # 알라딘 검색페이지 크롤링 함수
+    def __parse_searchdata(self):
         for i in self.get_items:
-            #Get Title
+            #책 제목 가져오기
             title = i.find("b", class_ = "bo3").text
 
-            #Get Description
+            #책 설명 가져오기
             tag_li = i.find_all("li")
             description = tag_li[1].text
 
-            #Get Shopinfo
+            #재고 있는 매장들 가져오기
             shop = {}
             tag_a = i.find_all("a", class_ = "usedshop_off_text3")
             for j in tag_a:
                 shopname = j.text
                 shopurl = j.attrs['href']
                 shop.setdefault(shopname, shopurl)
+
+            #HTML 소스코드 위에서 아래순으로 데이터들 리스트 추가
             self.list_title.append(title)
             self.list_description.append(description)
             self.list_shop.append(shop)
+
+    #데이터들 클래스 외부로 리턴
+    def return_data(self):
+        return self.list_title, self.list_description, self.list_shop
     
-    def print_data(self):
+    #데이터 잘 들어있나 확인
+    def print_searchdata(self):
         for i in range(0, self.item_quantity):
             print(self.list_title[i])
             print(self.list_description[i])
-            print(self.list_shop[i])
+            print(list(self.list_shop[i].keys()))
             print("--------------------------- \n")
 
-#Get singleitem info
-class itempage(Searchpage):
-    def __init__(self, keyword, num):
-        super().__init__(keyword)
-        self.title = self.list_title[num]
-        self.description = self.list_description[num]
-        self.target_shops = self.list_shop[num]
+#선택한 책에 대한 정보를 크롤링
+class Itempage:
+    def __init__(self, num, searchresult):
+        self.title = searchresult[0][num]
+        self.description = searchresult[1][num]
+        self.target_shops = searchresult[2][num]
         self.shops_stock = []
-        self.__parse_itemdata()   
+        self.__parse_itemdata()
 
+    #선택한 책의 매장별 가격과 위치 크롤링
     def __parse_itemdata(self):
         shopurl = self.target_shops.values()
+        shopname = list(self.target_shops.keys())
+
+        #각 각 매장에 있는 책의 정보 가져오기
+        loop_count = 0
         for i in shopurl:
             html = requests.get(i).text
             self.soup = BeautifulSoup(html, 'html.parser')
-            self.get_stock = self.soup.find_all("div", class_="ss_book_box")
+            self.get_stock = self.soup.find_all("div", class_="ss_book_box") #매장에 책 재고 있는 만큼 정보 가져오기
+            count_stock = len(self.get_stock)
             stock = []
             for j in self.get_stock:
-                info = []
-                price = j.find("span", class_="ss_p2").text
-                quality = j.find("span", class_="us_f_bob").text
-                location = j.find_all("span", class_="ss_p3")[3].find("b").text[7:].strip()
-                info.append(price)
-                info.append(quality)
-                info.append(location)
-                stock.append(info)
-            self.shops_stock.append(stock)
-    
+                price = j.find("span", class_="ss_p2").text #매장의 책 가격
+                quality = j.find("span", class_="us_f_bob").text.strip() #매장의 책 상태
+                location = j.find_all("span", class_="ss_p3")[3].find("b").text[7:].strip() #책이 있는 위치
+                
+                #매장안에 있는 책의 정보들 json으로 저장
+                item = {'가격' : price, '상태' : quality, '서적 위치' : location}
+                stock.append(item)
+            #매장 지점별로 책 재고 정보 저장
+            self.shops_stock.append({'지점' : shopname[loop_count], '재고 수량' : count_stock, '재고 현황' : stock})
+            loop_count += 1
+
+    #하나의 책에 대한 매장별로 재고 현황 표시
     def print_data(self):
-        print(self.title)
-        print(self.description)
-        print(self.target_shops.keys())
-        print(self.shops_stock)
+        print("책제목: " + self.title)
+        print("설명: " + self.description)
+        for i in range(0, len(self.shops_stock)):
+            print(json.dumps(self.shops_stock[i], indent=4, ensure_ascii = False))
 
+'''
 if __name__ == "__main__":
-    a = itempage("컴퓨터구조", 4)
-    a.print_data()
-
-    
-
-
-
-        
+    print("알라딘 오프라인 상점 검색기\n\n")
+    keyword = input("검색내용 입력: ")
+    print("\n")
+    a = Searchpage(keyword)
+    a.print_searchdata()
+    num = int(input("몇번째 아이템을 선택? : "))-1
+    print("\n")
+    b = Itempage(num, a.return_data())
+    b.print_data()
+'''
--- module/yes24.py
+++ module/yes24.py
@@ -1,2 +1,73 @@
+'''
+YES24 OfflineShop Parse Module by Sc0_Nep
+'''
 from bs4 import BeautifulSoup
 import requests
+import json
+
+#중고몰메인페이지에서 매장 어디어디 있는지 가져옴
+MAIN_URL = "http://www.yes24.com/Mall/UsedStore/Main"
+html = requests.get(MAIN_URL).text
+soup = BeautifulSoup(html, "html.parser")
+get_malls = soup.find("ul", id="ulStoreSerchCategory").find_all("li")
+malls = {} #중고서점 지점 : 중고서점 고유코드
+for i in get_malls:
+    malls.setdefault(i.text.strip(), i.find("a").attrs["value"])
+
+class Searchpage:
+    def __init__(self, keyword):
+        self.keyword = str(keyword.encode('ascii', 'backslashreplace')).upper().replace('\\\\U', '%u')[2:][:-1]
+        #매장별 검색결과 리스트
+        self.search_result = []
+        self.__parse_searchdata()
+    
+    def __parse_searchdata(self):
+        COMMON_URL = "http://www.yes24.com/Mall/UsedStore/Search?STORE_CODE="
+        mall_name = list(malls.keys())
+        mall_codes = list(malls.values())
+        for i in range(0, len(mall_name)):
+            source = requests.get(COMMON_URL + mall_codes[i] + "&searchText=" + self.keyword).text
+            soup = BeautifulSoup(source, "html.parser")
+            tag_ul = soup.find("ul", id = "ulResult")
+            result = []
+
+            #검색 결과 없는 경우도 있으니 확인
+            if str(tag_ul) == "None":
+                result.append("검색 결과 없음")
+            else:
+                tag_li = tag_ul.find_all("li")
+                for j in tag_li:
+                    #책 제목 가져오는 거
+                    title = j.find("strong", class_ = "name").text.strip()
+
+                    #설명부분 text들 싹다 가져와서 합쳐버리기 - description 크롤링
+                    tag_p = j.find("p", class_ = "storeG_pubGrp")
+                    tag_span = tag_p.find_all("span")
+                    description = "| "
+                    for k in tag_span:
+                        description += k.text + " | "
+
+                    #YES24는 매물에 가격또는 도서 위치가 없는 경우가 있음;;
+                    price = ""
+                    location = ""
+                    try:
+                        price = j.find("p", class_ = "storeG_price").text[2:].strip() #가격 부분 가져오기
+                        location = j.find("dd").text.strip() #책 위치 가져오기
+                    except AttributeError:
+                        price = "가격정보 없음"
+                        location = "위치정보 없음"
+
+                    #이제 이것들을 json 파일로 저장시켜버리자
+                    item = {'책 제목' : title, '설명' : description, '가격' : price, '서적 위치' : location}
+                    result.append(item)
+            self.search_result.append({'매장' : mall_name[i], '검색 결과' : result})
+
+    def print_data(self):
+        for i in range(0, len(self.search_result)):
+            print(json.dumps(self.search_result[i], indent=4, ensure_ascii = False))
+
+'''
+if __name__ == "__main__":
+    a = Searchpage("시스템")
+    a.print_data()
+''' 
